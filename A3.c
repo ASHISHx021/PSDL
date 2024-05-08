97f1312ea432530600c10ef5d264f2bedbe3c93a
ASHISHx021 <149313948+ASHISHx021@users.noreply.github.com>
2024-05-08 21:19:55 +0530

    updated working code

--- A3.c
+++ A3.c
@@ -1,18 +1,25 @@
 #include<pic18f4520.h> // Assuming you're using a PIC microcontroller and XC8 compiler
 
-void main(void) {
+void main() {
     int result = 0;
-    int num1= 0x03;
-    int num2= 0x010;
-    unsigned int i;
+    int num1= 3;
+    int num2= 10;
+    int num3 =4;
+    int num4 =8;
+    int i;
 
     // Step 3: Make PORTB as output port
     TRISB = 0;
+    TRISC = 0;
 
     // Step 2: Perform the multiplication
     for(i= 1; i <= num2; i++) {
         result = result + num1;
     }
+    
+    int num5 = num4/num3;
+    
+    PORTC = num5;
 
     // Step 4: Store the result in PORTB
     PORTB = result;
